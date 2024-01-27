e63a3b45fc2afa69e1b4681f07eec1889b891848
Felipe Moreno <felipe.moreno@fxinnovation.com>
2024-01-26 20:14:29 -0500

    feat(phone): return id of each row

--- 08_phone/insert.go
+++ 08_phone/insert.go
@@ -47,8 +47,13 @@ func createDB() (*sql.DB, error) {
 func populateDatabase(db *sql.DB) {
 	_, err := db.Exec("drop table phone_numbers") // as a former DBA this feels so right 😈
 	_, err = db.Exec("CREATE TABLE phone_numbers(ID SERIAL PRIMARY KEY, NUMBER TEXT NOT NULL)")
+	must(err)
 	for _, number := range phone.SampleData() {
-		_, err = db.Exec("INSERT INTO phone_numbers (NUMBER) values ($1)", number)
+		// result, err := db.Exec("INSERT INTO phone_numbers (NUMBER) values ($1)", number)
+		// id, err := result.LastInsertId()       // postgres doesn't support: panic: LastInsertId is not supported by this driver
+		var id int
+		err := db.QueryRow("INSERT INTO phone_numbers (NUMBER) values ($1) RETURNING id", number).Scan(&id)
+		fmt.Printf("id=%d\n", id)
 		must(err)
 	}
 }
