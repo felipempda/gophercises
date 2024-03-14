666ae1650382b36c78421900c53b3e13680ae307
Felipe Moreno <felipe.moreno@fxinnovation.com>
2024-03-13 23:32:34 -0400

    fix(blackjack): dealer turn after player

--- 10_blackjack/blackjack.go
+++ 10_blackjack/blackjack.go
@@ -51,17 +51,27 @@ func drawCard() deck.Card {
 }
 
 func play() {
+	//for i := 1; i<=4; i++ {
 	initialDraw()
 	for gameRound = 1; shouldContinue(); gameRound++ {
 		printScreen(false)
-		processDecision(waitPlayersDecision())
+		if !player1.stand {
+			playersTurn()
+		} else {
+			dealersTurn()
+		}
 	}
 	printScreen(true)
+	// }
 }
 
 func initialDraw() {
 	// start Deck
 	gameDeck = deck.New(deck.WithMultipleDecks(2), deck.Shuffle)
+	player1.cards = nil
+	player1.stand = false
+	dealer.cards = nil
+	dealer.stand = false
 
 	// draw 2 cards
 	for i := 0; i < 2; i++ {
@@ -80,22 +90,22 @@ func calculatePointsAllPlayers() {
 
 func shouldContinue() bool {
 	calculatePointsAllPlayers()
-	return !player1.stand && player1.points < 21 && dealer.points < 21
+	return !dealer.stand && player1.points < 21
 }
 
 func waitPlayersDecision() decision {
 	var decided decision
 	for true {
-		fmt.Printf("Press (1) for Hit or (2) for Stand: ")
+		fmt.Printf("Press (h)it or (s)tand: ")
 		reader := bufio.NewReader(os.Stdin)
 		got, _ := reader.ReadString('\n')
 		got = strings.Replace(got, "\n", "", -1)
 
-		if got == "1" {
+		if got == "h" {
 			fmt.Println("You chose Hit!")
 			decided = hit
 			break
-		} else if got == "2" {
+		} else if got == "s" {
 			fmt.Println("You chose Stand!")
 			decided = stand
 			break
@@ -115,28 +125,12 @@ func calculatePoints(p participant, upToPosition int) int {
 			break
 		}
 		var value int
-		switch card.Rank {
-		case deck.Ace:
-			value = 1
-			numberAces++
-		case deck.Two:
-			value = 2
-		case deck.Three:
-			value = 3
-		case deck.Four:
-			value = 4
-		case deck.Five:
-			value = 5
-		case deck.Six:
-			value = 6
-		case deck.Seven:
-			value = 7
-		case deck.Eight:
-			value = 8
-		case deck.Nine:
-			value = 9
-		default:
+		value = int(card.Rank)
+		if value > 10 {
 			value = 10
+		}
+		if card.Rank == deck.Ace {
+			numberAces++
 		}
 		total = total + value
 	}
@@ -151,15 +145,26 @@ func calculatePoints(p participant, upToPosition int) int {
 	return total
 }
 
-func processDecision(d decision) {
+func playersTurn() {
+	d := waitPlayersDecision()
 	if d == hit {
 		player1.cards = append(player1.cards, drawCard())
-		// drawACard
-		if shouldContinue() {
-			dealer.cards = append(dealer.cards, drawCard())
+		player1.points = calculatePoints(player1, -1)
+		if player1.points >= 21 {
+			player1.stand = true
 		}
 	} else {
 		player1.stand = true
+	}
+}
+
+func dealersTurn() {
+	// drawACard
+	if dealer.points <= 16 {
+		dealer.cards = append(dealer.cards, drawCard())
+		dealer.points = calculatePoints(dealer, -1)
+	} else {
+		dealer.stand = true
 	}
 }
 
@@ -173,34 +178,37 @@ func printScreen(endGame bool) {
 	// show dealer cards only at the end, at the beginning only first card is shown
 	var dealer_cards string
 	var dealer_points int
+	var roundText string
 	if endGame {
 		dealer_cards = Hand(dealer.cards).String()
 		dealer_points = calculatePoints(dealer, -1)
+		roundText = "END"
 	} else {
 		dealer_cards = Hand(dealer.cards[0:1]).String() + ", ** HIDDEN **"
 		dealer_points = calculatePoints(dealer, 0)
+		roundText = fmt.Sprintf("ROUND %d", gameRound)
 	}
 
-	fmt.Printf("[ BLACK JACK   -  ROUND %d ]\n\n", gameRound)
+	fmt.Printf("[ BLACK JACK  - %s ]\n\n", roundText)
 	fmt.Printf("------------------------------------------------------------------------------\n")
 	fmt.Printf(" Dealer (%d cards): %s = %d \n", len(dealer.cards), dealer_cards, dealer_points)
 	fmt.Printf(" Player (%d cards): %s = %d \n ", len(player1.cards), Hand(player1.cards), player1.points)
 	fmt.Printf("------------------------------------------------------------------------------\n")
 
 	if endGame {
-		var burst bool
-		if player1.points > 21 {
-			fmt.Println("PLAYER1 BURST")
-			burst = true
-		}
-		if dealer.points > 21 {
-			fmt.Println("DEALER BURST")
-			burst = true
-		}
-		if player1.points == 21 || (!burst && player1.points > dealer.points) {
-			fmt.Println("PLAYER1 WINS!")
-		} else if dealer.points == 21 || (!burst && dealer.points > player1.points) {
+		switch {
+		case player1.points > 21:
+			fmt.Println("PLAYER BURST!")
+		case dealer.points > 21:
+			fmt.Println("DEALER BURST!")
+		case player1.points > dealer.points:
+			fmt.Println("PLAYER WINS!")
+		case dealer.points > player1.points:
 			fmt.Println("DEALER WINS!")
+		case dealer.points == player1.points:
+			fmt.Println("DRAW!")
 		}
+		fmt.Println("Press enter to continue...")
+		bufio.NewReader(os.Stdin).ReadBytes('\n')
 	}
 }
